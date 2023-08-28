4b776af56c46193d81a34d6aebd1e9bd723b35c5
swetha3819 <108916586+swetha3819@users.noreply.github.com>
2023-08-28 09:50:58 +0530

    Add files via upload

---
+++ java/com/example/demo/Dayy5Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dayy5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dayy5Application.class, args);
+	}
+
+}
